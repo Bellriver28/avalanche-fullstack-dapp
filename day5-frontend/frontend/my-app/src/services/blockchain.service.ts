0c7c5158abfebbae35f063bc5b3f1e2bc48612d4
Bellriver <142145493+Bellriver28@users.noreply.github.com>
2026-01-19 11:42:32 +0700

    Update blockchain.service.ts

--- day5-frontend/frontend/my-app/src/services/blockchain.service.ts
+++ day5-frontend/frontend/my-app/src/services/blockchain.service.ts
@@ -1,5 +1,7 @@
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
+console.log("BACKEND_URL:", BACKEND_URL);
+
 if (!BACKEND_URL) {
   throw new Error("NEXT_PUBLIC_BACKEND_URL is not defined");
 }
