bde7b43e4d005b007878a7c9b7f7808f66b14459
Bellriver <142145493+Bellriver28@users.noreply.github.com>
2026-01-19 12:35:22 +0700

    Create page.tsx

---
+++ day5-frontend/frontend/my-app/app/dapp/page.tsx
@@ -0,0 +1,10 @@
+import Dapp from "@/app/component/Dapp";
+
+export default function DappPage() {
+  return (
+    <main className="p-6 min-h-screen bg-black text-white">
+      <h1 className="text-2xl font-bold mb-4">Dapp Page</h1>
+      <Dapp />
+    </main>
+  );
+}
